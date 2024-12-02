a629f3ffd0a9922a9b7e5e5ef2ad1304196552d3
zaki-lknr <zaki.hmkc+github@gmail.com>
2024-12-02 23:47:31 +0900

    add: profile/search/hashtagのリンク

--- r2tokimeki.js
+++ r2tokimeki.js
@@ -1,18 +1,35 @@
-console.log("Road to TOKIMEKI!");
+// console.log("Road to TOKIMEKI!");
 
 const link_items = document.getElementsByTagName("a");
-// console.log(link_items);
 for (const link_item of link_items) {
-    // console.log(link_item);
-    // const url = link_item.getAttribute('href');
     const url = link_item.href;
     // console.log(url);
     if (url.startsWith('https://bsky.app')) {
-        const tokimeki_url = url.replace('https://bsky.app', 'https://tokimeki.blue');
-        // console.log(tokimeki_url);
-        link_item.href = tokimeki_url;
-        console.log(link_item.href);
+        // const a = url.match(/https:\/\/bsky.app\/(.*)\//);
+        const a = /https:\/\/bsky.app\/(.*?)[\/\?]/.exec(url);
+        // console.log(a);
+        switch (a[1]) {
+            case "profile":
+                // プロフィールページ・各投稿ページ・フィード
+            case "search":
+                // 検索
+                link_item.href = url.replace('https://bsky.app', 'https://tokimeki.blue');
+                break;
+
+            case "hashtag":
+                // ハッシュタグ：TOKIMEKIは#付きの通常検索
+                link_item.href = url.replace('https://bsky.app/hashtag/', 'https://tokimeki.blue/search?q=%23');
+                break;
+
+            // case "messages":
+            //     // DM (要らない気もする?)
+            //     link_item.href = url.replace('https://bsky.app/messages', 'https://tokimeki.blue/chat');
+            //     break;
+            //// チャットのURLを直で開いてもデータがロードされないので機能オフ
+
+            default:
+                break;
+        }
+        // console.log("bluesky link: " + link_item.href);
     }
 }
-
-console.log("ext end");
