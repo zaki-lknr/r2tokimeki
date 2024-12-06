03fbfde4b57bff55b5bdcbfe8d8a090cc2d33eaa
zaki-lknr <zaki.hmkc+github@gmail.com>
2024-12-06 18:00:58 +0900

    add: profile, searchの遷移 #3

--- worker.js
+++ worker.js
@@ -21,5 +21,21 @@ const road2tokimeki = () => {
     // console.log("road2tokimeki");
     // console.log(target_url); // NG (letでも変わらず)
     // window.open("https://www.google.com"); // 別タブ
-    location.href = "https://www.google.com";
+    console.log(location.href);
+    if (location.href.startsWith('https://bsky.app')) {
+        console.log("r2tokimeki");
+        const bsky_url = location.href;
+        //TODO もっと上位(chrome拡張レイヤー)で処理する
+        const a = /https:\/\/bsky.app\/(.*?)[\/\?]/.exec(bsky_url);
+        switch (a[1]) {
+            case "profile":
+                // プロフィールページ・各投稿ページ・フィード
+            case "search":
+                // 検索
+                location.href = bsky_url.replace('https://bsky.app', 'https://tokimeki.blue');
+                break;
+        }
+    }
+
+    // location.href = "https://www.google.com";
 }
